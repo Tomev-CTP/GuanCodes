8d5c2d0ef66d8cd4e74d9b50df8537bee2826629
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2021-05-20 12:22:53 +0200

    feat(codes): Add a method for generating codes as tuples.

--- src/GuanCodeGenerator.py
+++ src/GuanCodeGenerator.py
@@ -7,7 +7,7 @@ __author__ = "Tomasz Rybotycki"
     don't have additional array/list entries for control.
 """
 
-from typing import List
+from typing import List, Tuple
 
 from numpy import ones, zeros
 
@@ -16,7 +16,7 @@ class GuanCodeGenerator:
         pass
 
     @staticmethod
-    def generate_guan_codes(positions_limits: List[int]):
+    def generate_guan_codes(positions_limits: List[int]) -> List[List[int]]:
 
         current_code = zeros(len(positions_limits), dtype=int)              # g
         code_update_information = ones(len(positions_limits), dtype=int)    # u
@@ -37,6 +37,35 @@ class GuanCodeGenerator:
                     return codes
 
                 updated_value_at_index = current_code[index_to_update] + code_update_information[index_to_update]
+
+            current_code[index_to_update] = updated_value_at_index
+
+        return codes
+
+    @staticmethod
+    def generate_guan_codes_tuples(positions_limits: List[int]) -> List[Tuple[int]]:
+        current_code = zeros(len(positions_limits), dtype=int)  # g
+        code_update_information = ones(len(positions_limits), dtype=int)  # u
+        position_limits = list(reversed(positions_limits))  # n
+
+        codes = []
+
+        while (current_code[-1] <= position_limits[-1]):
+            codes.append(tuple(reversed(current_code)))
+
+            index_to_update = 0  # i
+            updated_value_at_index = current_code[0] + code_update_information[0]  # k
+            while updated_value_at_index > position_limits[
+                index_to_update] or updated_value_at_index < 0:
+                code_update_information[index_to_update] = -code_update_information[
+                    index_to_update]
+                index_to_update += 1
+
+                if index_to_update == len(current_code):
+                    return codes
+
+                updated_value_at_index = current_code[index_to_update] + \
+                                         code_update_information[index_to_update]
 
             current_code[index_to_update] = updated_value_at_index
 
