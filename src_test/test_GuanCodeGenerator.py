0912805181d8065a768ac411321bd736844b04b9
Tomev <rybotycki.tomasz+github@gmail.com>
2021-12-03 12:03:59 +0100

    fix(tests): Fix test so that they can be run from submodules by pytest.

--- src_test/test_GuanCodeGenerator.py
+++ src_test/test_GuanCodeGenerator.py
@@ -4,7 +4,7 @@ __author__ = "Tomasz Rybotycki"
     This script contains quick test ground of the generator.
 """
 
-from src.GuanCodeGenerator import GuanCodeGenerator
+from ..src.GuanCodeGenerator import GuanCodeGenerator
 
 for code in GuanCodeGenerator.generate_guan_codes([4, 0, 3, 3, 3]):
     print(code)
